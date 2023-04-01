abc20646f09c50c2ea86c72aedd17a0cad5ab038
TalonEzio <vuthemanh1707@gmail.com>
2023-04-01 18:51:06 +0700

    header 50%

--- Script/main.js
+++ Script/main.js
@@ -30,3 +30,13 @@
 //         }
 //     )
 // });
+
+$(document).ready(function () {
+    $('.slider-list').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+
+    });
+});
